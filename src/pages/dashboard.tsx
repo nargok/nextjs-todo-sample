e24a2047aa7d78b84b8f828f5471f5088e352b47
nargok <nargok@gmail.com>
2023-05-07 14:03:44 +0900

    display user info and logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,10 +4,17 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '@/components/Layout'
+import { UserInfo } from '@/components/UserInfo'
 
 const DashBoard: NextPage = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
+
+  /**
+   * ログアウトする
+   */
   const logout = async () => {
+    queryClient.removeQueries(['user']) // cacheのクリア
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
@@ -18,6 +25,7 @@ const DashBoard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={logout}
       />
+      <UserInfo />
     </Layout>
   )
 }
